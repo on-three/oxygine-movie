fbde9febabecb58927c642552d1589551ed10d07
on-three <on.three.email@gmail.com>
2019-04-28 15:18:53 -0700

    Initial attempt at web build

---
+++ examples/Movie/proj.cmake/run.web.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BUILD_DIR=web-build
+TARGET=Movie.html
+
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+MAKE="emmake make"
+CMAKE="emcmake cmake"
+
+#generate cmake project in the "build" folder
+${CMAKE} ..
+
+#build it
+${MAKE} VERBOSE=1
+
+#move to working data folder with resources
+#cd ../../data
+
+#run executable
+#./../proj.cmake/build/Movie
+emrun ${TARGET}
